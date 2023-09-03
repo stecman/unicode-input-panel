121f128a94ec14022897828f23e7f9787b777993
Stephen Holdaway <stecman@gmail.com>
2023-09-03 13:02:37 +1200

    Clean up cruft in repository

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 scripts/render-codepoints.py -f data/fonts/ -o /tmp/fonts --metadata-font data/fonts/NotoSansMono-Regular.otf --code-data data/UnicodeData.txt --block-data data/Blocks.txt $@
